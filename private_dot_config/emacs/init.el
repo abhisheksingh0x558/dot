335fdaf2fec64a4fd2884e5857da41030243881b
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-30 22:15:06 +0530

    emacs: setup package manager

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -3,3 +3,24 @@
 ;; Elisp path
 (defvar my-elisp-path (expand-file-name "elisp" user-emacs-directory))
 (add-to-list 'load-path my-elisp-path) ; Custom modules
+
+;; Load package manager
+(setq elpaca-installer-version 0.11)
+(let ((elpaca-path (expand-file-name "elpaca/repos/elpaca" user-emacs-directory)))
+  (add-to-list 'load-path elpaca-path)
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" elpaca-path)))
+(require 'elpaca-autoloads)
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca (elpaca :repo "https://github.com/progfolio/elpaca.git" :build (:not elpaca--activate-package)))
+(elpaca leaf
+  (require 'leaf)
+  (setq leaf-defaults '(:elpaca t)))
+(elpaca leaf-keywords
+  (require 'leaf-keywords)
+  (leaf-keywords-init))
+(elpaca-wait)
+
+;; Setup packages
+(elpaca-wait)
