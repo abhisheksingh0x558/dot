b2048c4e3f1cca83e0f93d3070d0b476a2b77935
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-07-06 00:17:38 +0530

    nvim: configure haskell support

--- private_dot_config/nvim/lua/my/lang.lua
+++ private_dot_config/nvim/lua/my/lang.lua
@@ -63,6 +63,13 @@ local languages = {
 		linters = { "fennel" },
 		formatters = { "fnlfmt" },
 	},
+
+	-- Haskell
+	haskell = {
+		language_server = "hls",
+		linters = { "hlint" },
+		formatters = { "fourmolu" },
+	},
 }
 
 -- Setup language tools
