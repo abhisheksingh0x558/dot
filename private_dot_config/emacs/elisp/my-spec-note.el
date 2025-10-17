6366c679728dcbf963732fb244e66e82d3413d24
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-10-17 23:41:27 +0530

    emacs: setup note taker packages

---
+++ private_dot_config/emacs/elisp/my-spec-note.el
@@ -0,0 +1,7 @@
+;; Zettelkasten notes
+(leaf org-roam
+  :custom* ((org-roam-directory (expand-file-name "zettelkasten" org-directory)))) ; Org roam directory
+
+(leaf orgtbl-aggregate) ; Org table aggregations
+
+(provide 'my-spec-note)
