24b89fa70667bd9f0e6f4c016a0b009903a6b67b
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-27 13:13:32 +0530

    nvim: setup key mapping plugins

---
+++ private_dot_config/nvim/lua/my/spec/keymap.lua
@@ -0,0 +1,52 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			-- Manipulate indentations
+			scope = {
+				enabled = true,
+				keys = {
+					jump = {},
+				},
+			},
+		},
+	},
+
+	-- Manipulate comments
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+
+	-- Manipulate surrounding pairs
+	{
+		"kylechui/nvim-surround",
+		opts = {},
+	},
+
+	-- Exchange text
+	{
+		"gbprod/substitute.nvim",
+		opts = {},
+	},
+
+	-- Manipulate text cases
+	{
+		"johmsalas/text-case.nvim",
+		opts = {},
+	},
+
+	-- Navigate with search labels
+	{
+		"folke/flash.nvim",
+		opts = {},
+	},
+
+	"Vonr/align.nvim", -- Manipulate alignments
+
+	-- Find and replace in project
+	{
+		"MagicDuck/grug-far.nvim",
+		opts = {},
+	},
+}
