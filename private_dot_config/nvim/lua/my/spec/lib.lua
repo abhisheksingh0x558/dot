fe6dead67852dd33bc880b5a70065c318ddcda32
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-27 13:12:19 +0530

    nvim: setup lua libraries

---
+++ private_dot_config/nvim/lua/my/spec/lib.lua
@@ -0,0 +1,5 @@
+return {
+	"kevinhwang91/promise-async", -- TODO: Use luarocks to manage this and remove from here
+
+	"nvim-neotest/nvim-nio", -- TODO: Use luarocks to manage this and remove from here
+}
