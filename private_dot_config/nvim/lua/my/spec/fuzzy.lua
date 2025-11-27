a5dd1a0e1764fe31edb8f2dc5d2a71bc842ff6cf
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-27 13:20:27 +0530

    nvim: setup fuzzy finder plugins

---
+++ private_dot_config/nvim/lua/my/spec/fuzzy.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				enabled = true,
+				layout = {
+					preset = "bottom", -- Show fuzzy finder at the bottom
+				},
+				sources = {
+					files = {
+						hidden = true, -- Show hidden files
+					},
+					grep = {
+						hidden = true, -- Show hidden files
+					},
+				},
+			},
+		},
+	},
+}
