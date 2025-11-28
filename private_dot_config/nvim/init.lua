58a7d56904de2bb4cc2a6234712c8c1cbe69c288
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-28 15:36:21 +0530

    nvim: configure key mappings

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -2,5 +2,8 @@
 vim.opt.runtimepath:prepend(vim.fs.normalize("~/.local/share/nvim/lazy/lazy.nvim"))
 local lazy = require("lazy")
 
+-- Load core modules
+require("my.core.keymap")
+
 -- Setup plugins
 lazy.setup()
