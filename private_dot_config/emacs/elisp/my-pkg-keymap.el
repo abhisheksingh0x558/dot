8f237e214ede8a52869fd23435875b480adaef2a
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-12-03 00:52:29 +0530

    emacs: configure key mapping packages

---
+++ private_dot_config/emacs/elisp/my-pkg-keymap.el
@@ -0,0 +1,13 @@
+;; Vi layer
+(evil-mode)
+
+(evil-set-leader 'normal (kbd "SPC")) ; Leader key
+(evil-set-leader 'normal "\\" t) ; Local leader key
+
+(evil-collection-init) ; Vi mappings across modes
+
+(evil-commentary-mode) ; Manipulate comments
+
+(global-evil-surround-mode) ; Manipulate surrounding pairs
+
+(provide 'my-pkg-keymap)
