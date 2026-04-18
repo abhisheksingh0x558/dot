94ba62b7e20537b68e479eb31f90281ecf9d5a3b
Abhishek Singh <abhisheksingh0x558@proton.me>
2026-04-19 04:20:41 +0530

    emacs: wip

--- private_dot_config/emacs/early-init.el
+++ private_dot_config/emacs/early-init.el
@@ -1,9 +1,3 @@
 ;; -*- lexical-binding: t -*-
 
-;; Package manager
-(setq package-enable-at-startup nil) ; Disable package loading at startup
-
-;; UI/UX
-(menu-bar-mode 0) ; Disable menu bar
-(tool-bar-mode 0) ; Disable tool bar
-(scroll-bar-mode 0) ; Disable scroll bar
+(provide 'early-init)
