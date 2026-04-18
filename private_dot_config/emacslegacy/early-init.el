cefde29a1677ffd0a87a29e632af50cb80629f8d
Abhishek Singh <abhisheksingh0x558@proton.me>
2026-04-19 04:19:18 +0530

    emacslegacy: wip

---
+++ private_dot_config/emacslegacy/early-init.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t -*-
+
+;; Package manager
+(setq package-enable-at-startup nil) ; Disable package loading at startup
+
+;; UI/UX
+(menu-bar-mode 0) ; Disable menu bar
+(tool-bar-mode 0) ; Disable tool bar
+(scroll-bar-mode 0) ; Disable scroll bar
