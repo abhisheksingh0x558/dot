fd28737d77333d38a9b4305b9f708ded9fe21ea1
Abhishek Singh <abhisheksingh0x558@proton.me>
2026-06-26 16:54:15 +0530

    wip

--- private_dot_config/nvimlegacy/lua/my/spec/lib.lua
+++ private_dot_config/nvimlegacy/lua/my/spec/lib.lua
@@ -1,4 +1,6 @@
 return {
+	"nvim-lua/plenary.nvim", -- TODO: Use luarocks to manage this and remove from here
+
 	"kevinhwang91/promise-async", -- TODO: Use luarocks to manage this and remove from here
 
 	"nvim-neotest/nvim-nio", -- TODO: Use luarocks to manage this and remove from here
